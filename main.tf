cda8efaa7218365c87743a911116c80ef83d4430
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-14 13:09:57 +0200

    creating VPC

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ terraform {
         aws = {
             source = "hashicorp/aws"
             version = "5.94.1"
+
         }
     }
+}
+
+# Creating VPC
+resource "aws_vpc" "wordpress-vpc" {
+  cidr_block = "10.0.0.0/16"
 }
