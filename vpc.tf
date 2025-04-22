661087e3f31c30eac08160828388b747a9c72c5d
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-22 14:24:47 +0200

    adding public and private subnet for upcoming  wordpress instance

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "wordpress-vpc" {
 
 resource "aws_subnet" "pub1-wordpress" {
   cidr_block        = "10.0.0.0/28"
-  availability_zone = "eu-north-1a"
+  availability_zone = "us-west-2-1a"
   vpc_id            = aws_vpc.wordpress-vpc.id
   
   tags = {
@@ -20,7 +20,7 @@ resource "aws_subnet" "pub1-wordpress" {
 resource "aws_subnet" "priv1-wordpress" {
   vpc_id            = aws_vpc.wordpress-vpc.id
   cidr_block        = "10.0.1.0/28"
-  availability_zone = "eu-north-1a"
+  availability_zone = "us-west-2-1a"
 
   tags = {
     Name = "priv1-wordpress"
