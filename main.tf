403b34ecbbb02cd144165496733aa7210cd6ff57
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-22 13:51:35 +0200

    outsourced the code to vpc.tf file

--- main.tf
+++ main.tf
@@ -3,16 +3,6 @@ terraform {
         aws = {
             source = "hashicorp/aws"
             version = "5.94.1"
-
         }
     }
-}
-
-# Creating VPC
-resource "aws_vpc" "wordpress-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "wordpress-vpc"
-  }
 }
---
+++ vpc.tf
@@ -0,0 +1,28 @@
+# Creating VPC
+resource "aws_vpc" "wordpress-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "wordpress-vpc"
+  }
+}
+
+resource "aws_subnet" "pub1-wordpress" {
+  cidr_block        = "10.0.0.0/28"
+  availability_zone = "eu-north-1a"
+  vpc_id            = aws_vpc.wordpress-vpc.id
+  
+  tags = {
+    Name = "pub1-wordpress"
+  }
+}
+
+resource "aws_subnet" "priv1-wordpress" {
+  vpc_id            = aws_vpc.wordpress-vpc.id
+  cidr_block        = "10.0.1.0/28"
+  availability_zone = "eu-north-1a"
+
+  tags = {
+    Name = "priv1-wordpress"
+  }
+}
