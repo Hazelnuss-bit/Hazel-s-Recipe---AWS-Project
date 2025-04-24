a9604675749ebd8db8d7efd07c8be3b43a5d925c
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-24 21:47:43 +0200

    removed public subnet and added in public_subnet.tf file

--- vpc.tf
+++ vpc.tf
@@ -6,13 +6,3 @@ resource "aws_vpc" "wordpress-vpc" {
     Name = "wordpress-vpc"
   }
 }
-
-resource "aws_subnet" "pub1-wordpress" {
-  cidr_block        = "10.0.0.0/28"
-  availability_zone = "us-west-2a"
-  vpc_id            = aws_vpc.wordpress-vpc.id
-  
-  tags = {
-    Name = "pub1-wordpress"
-  }
-}
