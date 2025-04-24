0cc2c480661d2534a85740ef5ace66bfff967ced
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-24 22:07:42 +0200

    Public Subnet was taken from vpc.tf file and placed inside this file

---
+++ public_subnet.tf
@@ -0,0 +1,10 @@
+# Creating Public Subnet
+resource "aws_subnet" "pub1-wordpress" {
+  cidr_block        = "10.0.0.0/28"
+  availability_zone = "us-west-2a"
+  vpc_id            = aws_vpc.wordpress-vpc.id
+  
+  tags = {
+    Name = "wordpress-publicsubnet"
+  }
+}
