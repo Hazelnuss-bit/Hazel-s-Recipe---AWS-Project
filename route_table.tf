d8229137921f79cf913ed688967516b16f7a202f
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-24 22:42:36 +0200

    Fix naming inconsistencies for VPC and subnet references in route table setup

--- route_table.tf
+++ route_table.tf
@@ -1,18 +1,18 @@
-# Route Table
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
+    # Route Table
+    resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.wordpress-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
   tags = {
     Name = "wordpress-rtb"
   }
 }
 
-# Associate Subnet with Route Table
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.pub1-wordpress.id
+  subnet_id      = aws_subnet.pub1_wordpress.id
   route_table_id = aws_route_table.public_rt.id
 }
