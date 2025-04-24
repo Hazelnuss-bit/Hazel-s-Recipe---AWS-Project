8141c6aff8d143279ee445bcadd0206269de15a8
Hazelnuss <gregoriohazelanne21@gmail.com>
2025-04-24 22:53:43 +0200

    Fix Correct subnet resource name to match reference in route table

--- route_table.tf
+++ route_table.tf
@@ -13,6 +13,6 @@
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.pub1_wordpress.id
+  subnet_id      = aws_subnet.pub1-wordpress.id
   route_table_id = aws_route_table.public_rt.id
 }
